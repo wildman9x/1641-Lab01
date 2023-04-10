9d3373af8b740c2f8b764dcc6b7f5d5c836e22a1
wildman9x <wildmanyt@gmail.com>
2023-04-10 19:27:11 +0700

    update

--- Lab5-func.py
+++ Lab5-func.py
@@ -12,27 +12,44 @@ def main():
         if choice == '0':
             break
         menu_chosen(choice)
+        
+funcs = {}
+def menu_item(item_func):
+    funcs[len(funcs)] = [item_func, item_func.__doc__]
 
+@menu_item
+def exit():
+    """Exit"""
+    pass
+
+@menu_item
 def AddProduct():
+    """Add a product"""
     name = input("Enter product name: ")
     price = input("Enter product price: ")
     productId = uuid.uuid4()
     productList.append([productId, name, price])
     print("Product added successfully")
 
+@menu_item
 def ProductList():
+    """Product list"""
     for product in productList:
         print(f'Name: {product[1]} \n Price: {product[2]} \n ID: {product[0]}')
-        
+
+@menu_item
 def FindProductByName():
+    """Find product by name"""
     name = input("Enter product name: ")
     for product in productList:
         if product[1] == name:
             print(f'Name: {product[1]} \n Price: {product[2]} \n ID: {product[0]}')
             return
     print("Product not found")
-    
+
+@menu_item
 def FindProductByPrice():
+    """Find product by price"""
     print("1. Less than",
           "2. Greater than",
           sep='\n')
@@ -49,8 +66,10 @@ def FindProductByPrice():
                 print(f'Name: {product[1]} \n Price: {product[2]} \n ID: {product[0]}')
     else:
         print("Invalid choice")
-        
+
+@menu_item
 def ChangeProductPrice():
+    """Change product price"""
     productId = input("Enter product ID: ")
     for product in productList:
         if str(product[0]) == productId:
@@ -58,8 +77,10 @@ def ChangeProductPrice():
             print("Price changed successfully")
             return
     print("Product not found")
-    
+
+@menu_item
 def DeleteProduct():
+    """Delete product"""
     productId = input("Enter product ID: ")
     for product in productList:
         if product[0] == productId:
@@ -67,17 +88,14 @@ def DeleteProduct():
             print("Product deleted successfully")
             return
     print("Product not found")
-    
 
-funcs = {
-    '0': [exit, 'Exit'],
-    '1': [AddProduct, 'Add a product'],
-    '2': [ProductList, 'Product list'],
-    '4': [FindProductByName, 'Find product by name'],
-    '5': [FindProductByPrice, 'Find product by price'],
-    '6': [ChangeProductPrice, 'Change product price'],
-    '7': [DeleteProduct, 'Delete product'],
-}
+@menu_item
+def SumOfPrices():
+    """Sum of prices"""
+    sum = 0
+    for product in productList:
+        sum += product[2]
+    print(f'Sum of prices: {sum}')
 
 def print_menu():
     for item in funcs:
